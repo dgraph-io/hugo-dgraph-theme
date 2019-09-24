7010612053ac0ce4240cab9a70851d3a69011d64
Paul Korzhyk <paul@dgraph.io>
2019-09-24 19:41:45 +0300

    Kick old mailchimp code

--- static/js/custom.js
+++ static/js/custom.js
@@ -72,6 +72,30 @@ var intermediateValNotFunc = function () {
   var FLAG_COOKIE = 'noGithubEngage';
   var NUM_DAYS_COOKIE = 'disableGithubCounterDays';
 
+  function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+  }
+
+  function setCookie(cname, cvalue, exdays) {
+    exdays = exdays || 60;
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";domain=.dgraph.io;path=/";
+  }
+
   function gaEvent(cat, action, label, value) {
     if (!window.ga) {
       console.warn('no ga', arguments)
