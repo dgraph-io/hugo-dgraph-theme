76bb560b79225418c08d8e92282c315c6c2a5586
Ahmed El Bannan <akelbann@gmail.com>
2020-01-12 23:29:58 -0700

    Moved search to search.js, better search look

--- static/js/search.js
+++ static/js/search.js
@@ -1,4 +1,31 @@
+let showSearch = true;
+let widgetContainer = {};
+
+// Toggles search interface's visibility
+const toggleVisibility = () => {
+	widgetContainer.style.display = showSearch? "none" : "";
+	widgetContainer.querySelector('input').focus();
+	showSearch = !showSearch;
+}
+
 const parseSummary = (summary) => summary.trim().replace("<p>", "").replace("</p>", "");
+
+// Renders tags
+const renderTags = tags => `
+	<div class="tag-wrapper">
+	${
+		tags.map(tag => `
+			<span 
+				class="tag"
+				onclick="event.stopPropagation(); window.location.href = '/tags/${tag.toLowerCase()}'"
+			>
+				${tag}
+			</span>
+		`)
+		.join('')
+	}
+	</div>
+`;
 
 // Helper for the render function
 const renderIndexListItem = ({ indexId, hits }) => `
@@ -6,9 +33,6 @@ const renderIndexListItem = ({ indexId, hits }) => `
 	<div class="results-header">
 		<div class="number-of-results">
 			${hits.length} results found.
-		</div>
-		<div class="close-button">
-			XXXX
 		</div>
 	</div>
 
@@ -21,9 +45,15 @@ const renderIndexListItem = ({ indexId, hits }) => `
 						class="search-listing"
 						onclick="window.location.href='${hit.permalink}'"
 					>
-					<div class="title">${instantsearch.highlight({ attribute: 'title', hit })}</div>
-					<div class="summary">${parseSummary(hit.summary)} </div>
-					<div>${hit.tags}</div>
+					<div class="title">
+						${instantsearch.highlight({ attribute: 'title', hit })}
+					</div>
+					<div class="summary">
+						${parseSummary(hit.summary)} 
+					</div>
+					<div>
+						${renderTags(hit.tags || [])}
+					</div>
 				</li>`
 			)
 			.join('')}
@@ -36,33 +66,41 @@ const renderAutocomplete = (renderOptions, isFirstRender) => {
 	const { indices, currentRefinement, refine, widgetParams } = renderOptions;
 
 	if (isFirstRender) {
+		const background = document.createElement('div');
 		const input = document.createElement('input');
 		const ul = document.createElement('ul');
 		
+		background.className = "search-background";
 		ul.className = "search-results";
 		input.className = "search-input";
+
 		input.placeholder = "Search...";
+
+		background.onclick = toggleVisibility;
 
 		input.addEventListener('input', event => {
 			refine(event.currentTarget.value);
 		});
 
-		widgetParams.container.appendChild(input);
-		widgetParams.container.appendChild(ul);
+		widgetContainer = widgetParams.container;
+
+		widgetContainer.appendChild(background);
+		widgetContainer.appendChild(input);
+		widgetContainer.appendChild(ul);
+
+		toggleVisibility();
 	}
 
-	widgetParams.container.querySelector('input').value = currentRefinement;
-
-	const showSearchResults = widgetParams.container.querySelector('input') === document.activeElement;
+	widgetContainer.querySelector('input').value = currentRefinement;
 	
-	if (currentRefinement && showSearchResults) {
-		widgetParams.container.querySelector('ul').style.display = "";
-		widgetParams.container.querySelector('ul').innerHTML = indices
-		.map(renderIndexListItem)
-		.join('');
+	if (currentRefinement) {
+		widgetContainer.querySelector('ul').style.display = "";
+		widgetContainer.querySelector('ul').innerHTML = indices
+			.map(renderIndexListItem)
+			.join('');
 	} else {
-		widgetParams.container.querySelector('ul').innerHTML = "";
-		widgetParams.container.querySelector('ul').style.display = "none"
+		widgetContainer.querySelector('ul').innerHTML = "";
+		widgetContainer.querySelector('ul').style.display = "none"
 	}
 };
 
@@ -72,10 +110,9 @@ const createSearch = () => {
 		renderAutocomplete
 	);
 
-
-
+	// Algolia client
 	const client = algoliasearch('ACO13Q5JXW', '140be40b311ca64b71564f19bb2f0995');
-	// const index = client.initIndex('/');
+	
 	const search = instantsearch({
 		indexName: 'dgraph_blog',
 		searchClient: client,
